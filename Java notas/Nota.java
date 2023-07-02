7492ef6d8ae5a3aeb38f4d5dec0d5d081defcc26
Guilherme Bloemer <100363831+guilhermeblmr@users.noreply.github.com>
2023-07-02 18:05:55 -0300

    Adicionando novos arquivos
    Arquivos da nova versão do projeto, contendo correções de bugs e testes unitários.

---
+++ Java notas/Nota.java
@@ -0,0 +1,21 @@
+public class Nota {
+    private String titulo;
+    private String conteudo;
+
+    public Nota(String titulo, String conteudo) {
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+}
